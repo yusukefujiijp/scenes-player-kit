eccdb8b3b7ea9d2561bade5553120991eec87f49
yusukefujiijp <108225412+yusukefujiijp@users.noreply.github.com>
2026-07-06 20:22:00 +0900

    Delete js/final-audio-consumer-test.js

--- js/final-audio-consumer-test.js
+++
@@ -1,8 +0,0 @@
-let activeAudio = null;
-function clearAudio(){
-  if (activeAudio) {
-    try { activeAudio.pause(); } catch (_) {}
-    activeAudio = null;
-  }
-}
-window.addEventListener('pagehide', clearAudio, true);
